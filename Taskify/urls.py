aa7ec4a66a06240514ece574d2cbe40faafa485b
SHAMMAS <muhdshammas612@gmail.com>
2025-03-18 16:52:20 +0530

    solved error in urls and index function added on views [project]

--- Taskify/urls.py
+++ Taskify/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from .views import index
+from Taskify import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
