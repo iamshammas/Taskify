af2950f1e1d1557150e954efd47ecb68e1f67423
SHAMMAS <muhdshammas612@gmail.com>
2025-03-18 17:11:20 +0530

    signup function completed

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,18 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
 # Create your views here.
 
 def signup(requests):
+    if requests.method == 'POST':
+        unm = requests.POST.get('unm')
+        email = requests.POST.get('email')
+        pwd = requests.POST.get('pwd')
+        userr = User.objects.create_user(username=unm,email=email,password=pwd)
+        userr.save()
+        return redirect('loginn')
+    else:
+        pass
     return render(requests,'signup.html')
 
 def loginn(requests):
