cac09f4f1467d127bccb37163f206cc6d2101bba
SHAMMAS <muhdshammas612@gmail.com>
2025-03-18 16:53:48 +0530

    urls[project] updated to connect urls of todo [app]

--- Taskify/urls.py
+++ Taskify/urls.py
@@ -20,5 +20,6 @@ from Taskify import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('todo/',include('todo.urls'))
 ]
